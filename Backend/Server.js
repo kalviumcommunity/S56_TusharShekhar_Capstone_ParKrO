83fa41b7b6886cc9c254407dc28f5495a3dff4fd
codingwithustushar <shekhar.tushar1198@gmail.com>
2024-04-15 10:53:53 +0530

    updated main

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const uri = process.env.URI;
+const {UserDetails} = require('./User.js')
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+app.get('/login',async(req,res)=>{
+  try{
+    const data = await UserDetails.find()
+    res.json(data);
+  }
+  catch(error){
+    console.log(error)
+  }
+})
+app.post('/addUsers',async(req,res)=>{
+  try{
+    const response = await UserDetails.create(req.body)
+    res.status(200).json(response);
+  }
+  catch(error){
+    console.log(error);
+  }
+})
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
